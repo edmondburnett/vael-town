c3394f1b5d171039f2a805e30b0eeb6aef06e924
edmondburnett <_@edmondburnett.com>
2023-09-21 16:52:29 -0700

    Make emoji picker grayscale again for v4.2.0

--- app/javascript/mastodon/features/compose/components/emoji_picker_dropdown.jsx
+++ app/javascript/mastodon/features/compose/components/emoji_picker_dropdown.jsx
@@ -276,6 +276,7 @@ class EmojiPickerMenuImpl extends PureComponent {
     return (
       <div className={classNames('emoji-picker-dropdown__menu', { selecting: modifierOpen })} style={style} ref={this.setRef}>
         <EmojiPicker
+          theme='dark'  // vael
           perLine={8}
           emojiSize={22}
           sheetSize={32}
