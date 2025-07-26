f4da39a1a3ec82cfbfcb8d9d7755551ca650d224
Edmond Burnett <_@edmondburnett.com>
2025-07-26 11:47:46 -0700

    remove unused files

--- config/initializers/source.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-module Mastodon
-  module Version
-    module_function
-    def source_base_url
-      'https://github.com/edmondburnett/vael-town'
-    end
-  end
-end
